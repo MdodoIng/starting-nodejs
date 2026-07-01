9c7837cb71924d0542e15fa83b6a670adab9e2f8
ameen-lsquared <ameen.lsquared@gmail.com>
2026-07-01 18:17:09 +0530

    hhhs

--- src/orders/dto/create-order.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsArray, IsUUID } from 'class-validator';
-
-export class CreateOrderDto {
-  @IsArray()
-  @IsUUID('all', { each: true })  // validates every item in array is a UUID
-  productIds: string[];
-}
